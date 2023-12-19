72f4f8d0e7e16046012f9638b448e202a0914c0c
Npvpr <nlmnever2@gmail.com>
2023-12-20 01:58:02 +0630

    finished header and tools

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <div>
+            <div className="mb-3">Hi I am <b className="text-4xl lg:text-7xl">Naing Lin Maung</b></div>
+            <div>I am an enthusiastic computer science student with a passion for developing applications.</div>
+        </div>
+    )
+}
---
+++ components/Tools.js
@@ -0,0 +1,15 @@
+export default function Tools() {
+  return (
+    <>
+      <div className="text-xl mb-2 lg:text-3xl ">Tools I am familiar with:</div>
+      <ul className="text-base list-disc ml-5">
+        <li>HTML, CSS, JavaScript</li>
+        <li>Bootstrap, TailwindCSS</li>
+        <li>Node.js, Python</li>
+        <li>React, Next.js, Django</li>
+        <li>Django REST framework</li>
+        <li>PostgreSQL, MongoDB</li>
+      </ul>
+    </>
+  );
+}
